7085dd391f667cf79376939fbfb7ff975c7d3c8a
olivier.ndeye <oliviersndeye@gmail.com>
2022-01-10 17:48:52 -0300

    update realizado em velha.js

--- velha.js
+++ velha.js
@@ -1,6 +1,29 @@
 
 let jogador = null;
+let jogadorSelecionado = document.getElementById('jogador-selecionado')
+
+mudarJogador ('X')
 
 function escolherQuadrado(id){
-  console.log(id)
+  
+  let quadrado = document.getElementById(id);
+
+  quadrado.innerHTML = jogador;
+  quadrado.style.color = '#000';
+   if (quadrado.innerHTML !== '*'){
+   	return;
+   }
+
+   if (jogador === 'X'){
+   	 jogador = 'O';
+   } else {
+   	 jogador = 'X'
+   }
+   mudarJogador(jogador);
+}
+
+//função para receber o valor da label "jogador", e alterar para o oposto
+function mudarJogador(valor){
+  jogador = valor;
+  jogadorSelecionado.innerHTML = jogador; 
 }
